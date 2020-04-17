8b0a767fb1b2fc7598eabe342e89109cc971c93f
kahlil iman <kahlil.iman@gmail.com>
2020-04-17 16:38:13 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+  },
+  {
+    name: "Mountain",
+    image:
+      "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg",
+  },
+];
+
+app.get("/", function (req, res) {
+  res.render("landing");
+});
+
+app.get("/campgrounds", function (req, res) {
+  res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function (req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function (req, res) {
+  res.render("new");
+});
+
+app.listen(1234, function () {
+  console.log("Yelpcamp has started");
+});
