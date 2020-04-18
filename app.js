4a7de5169c8606efaadb5dba55283ff468e40481
kahlil iman <kahlil.iman@gmail.com>
2020-04-18 13:54:57 +0700

    installing mongoose

--- app.js
+++ app.js
@@ -1,47 +1,73 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
+
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+  useNewUrlParser: true,
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-var campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-  },
-  {
-    name: "Mountain",
-    image:
-      "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg",
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-  },
-  {
-    name: "Mountain",
-    image:
-      "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg",
-  },
-];
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Mountain",
+//     image:
+//       "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg",
+//   },
+//   function (err, camp) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Add new campground");
+//       console.log(camp);
+//     }
+//   }
+// );
 
 app.get("/", function (req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find(function (err, camps) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", { campgrounds: camps });
+    }
+  });
 });
 
 app.post("/campgrounds", function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
+  // var newCampground = { name: name, image: image };
+  // campgrounds.push(newCampground);
+  Campground.create(
+    {
+      name: name,
+      image: image,
+    },
+    function (err, camp) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Add new campground");
+        console.log(camp);
+      }
+    }
+  );
   res.redirect("/campgrounds");
 });
 
